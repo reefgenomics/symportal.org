673198611638c56e2784b22591a7d6e0b436c24d
Yulia Iakovleva <yulia.iakovleva@uni-konstanz.de>
2023-07-14 12:30:45 +0200

    deploy script

---
+++ docker_stack_deploy.sh
@@ -0,0 +1,9 @@
+# Remove previous builds
+docker stack rm symportal
+
+# Sleep for several seconds to make sure all containers are removed
+sleep 15
+
+# Deploy
+export $(xargs < .env)
+docker stack deploy --compose-file docker-compose.yml symportal
